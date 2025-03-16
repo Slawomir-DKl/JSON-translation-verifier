74a2d76d6b0eeb5e1a42132227842af08d2c44da
slawekdk <slawomir.duda-klimaszewski@dataedo.com>
2025-03-16 19:30:47 +0100

    fix: change function from foreach to for

--- main.ts
+++ main.ts
@@ -3,20 +3,45 @@ import * as fs from "fs";
 function checkDifferences(sourceFileName: string, targetFileName: string) {
   const enJsonString = fs.readFileSync(`./${sourceFileName}`, "utf-8");
   const plJsonString = fs.readFileSync(`./${targetFileName}`, "utf-8");
-
-  compareMainKeys(enJsonString, plJsonString);
-  compareMainKeys(plJsonString, enJsonString);
+  const enJsonData = JSON.parse(enJsonString);
+  const plJsonData = JSON.parse(plJsonString);
+  compareKeys("EN", enJsonData, "PL", plJsonData);
+  compareKeys("PL", plJsonData, "EN", enJsonData);
 }
 
-function compareMainKeys(sourceJSON: string, targetJSON: string) {
-  const sourceData = JSON.parse(sourceJSON);
-  const targetData = JSON.parse(targetJSON);
-  let comparer: string;
+function compareKeys(
+  sourceLanguage: string,
+  sourceData: Object,
+  targetLanguage: string,
+  targetData: Object
+) {
+  let comparator = false;
+  let sourceValue: any;
+  let targetValue: any;
 
-  Object.keys(sourceData).forEach((sourceKey) => {
-    comparer = Object.keys(targetData).indexOf(sourceKey) > -1 ? "✅" : "❌";
-    console.log(sourceKey, comparer);
-  });
+  for (const sourceKey in sourceData) {
+    for (const targetKey in targetData) {
+      if (sourceKey === targetKey) {
+        comparator = true;
+        sourceValue = sourceData[sourceKey];
+        targetValue = targetData[targetKey];
+        if (typeof sourceValue === "object" && sourceValue !== null) {
+          if (typeof targetValue === "object" && targetValue !== null) {
+            console.log(sourceKey, "OK");
+            // funkcja rekurencyjna
+          } else {
+            console.log(
+              `❌ Key ${sourceKey} is object in ${sourceLanguage} but not in ${targetLanguage}`
+            );
+          }
+        }
+      }
+    }
+    if (comparator === false) {
+      console.log(`❌ Key ${sourceKey} not found in ${targetLanguage}`);
+    }
+    comparator = false;
+  }
 }
 
 checkDifferences("en.json", "pl.json");
